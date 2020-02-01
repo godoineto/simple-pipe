394d749e6acb1d3e38b74d75b3761661d9dbb285
José Luiz de Godoi Neto <netogodoi.dev@gmail.com>
2020-02-01 19:19:58 -0300

    Inicializando projeto

---
+++ src/main/java/com/godoineto/simplepipe/SimplepipeApplication.java
@@ -0,0 +1,13 @@
+package com.godoineto.simplepipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplepipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplepipeApplication.class, args);
+	}
+
+}
